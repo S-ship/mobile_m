8b79c67c111ba528c5079059ddc3d521980c90c8
孙宇 <sunny.163.com>
2021-01-17 19:33:28 +0800

    添加API

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,19 @@
 <template>
+    <div>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>\
+    <div class="big"></div>
     <router-view/>
+    </div>
 </template>
 
 <style lang="less">
+.big {
+  width: 200px;
+  height: 200px;
+  background-color: pink;
+}
 </style>
